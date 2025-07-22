e2862bcd62a5d3a6618d75e6009f39b06b1f7eec
Finett0 <finettogs@gmail.com>
2025-07-22 07:25:24 -0300

    closure

--- conceitos/HOF-clousure.js
+++ conceitos/HOF-clousure.js
@@ -1,4 +1,4 @@
-//Higer Order Function ou HOF recebe uma função como argumento e retorna uma função comoresultado
+//Higer Order Function ou HOF recebe uma função como argumento e retorna uma função como resultado
 
 function somar({ x, y }) {
   return x + y;
@@ -14,3 +14,17 @@ function aplicarOperacao({ numero1, numero2, operacao }) {
 
 console.log(aplicarOperacao({ numero1: 5, numero2: 3, operacao: somar }));
 console.log(aplicarOperacao({ numero1: 5, numero2: 3, operacao: subtrair }));
+
+// implementação de closure
+// Uma closure é uma função que "lembra" das variáveis do lugar onde foi criada.
+
+function criarMultiplicador(fator) {
+  return function (numero) {
+    return numero * fator;
+  };
+}
+
+const multiplicador2 = criarMultiplicador(2);
+const multiplicador5 = criarMultiplicador(5);
+console.log("Resultado de multiplicador 8 por 2", multiplicador2(8));
+console.log("Resultado de multiplicador 8 por 5", multiplicador5(5));
