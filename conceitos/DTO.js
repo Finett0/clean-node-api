05475dad26b54080b3fed947eb498f6f468a3240
Finett0 <finettogs@gmail.com>
2025-07-21 22:04:26 -0300

    conceitos

---
+++ conceitos/DTO.js
@@ -0,0 +1,3 @@
+// DTO é um padrão de design utilizado para transferir dados entre camadas de uma aplicação.
+//O Objetivo principal  do DTO é encapsular um conjunto de dados em um objeto simples para reduzir a quantidade e informações transferidas
+// Com isso o desempenho da comunicação melhora
